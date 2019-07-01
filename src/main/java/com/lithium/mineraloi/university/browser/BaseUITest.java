effbfc075f9846cca0ea44bda4f530a3df98c687
= <jeffrey.leung@live.com>
2019-07-01 13:04:00 -0500

    twitter project integration test

--- src/main/java/com/lithium/mineraloi/university/browser/BaseUITest.java
+++ src/main/java/com/lithium/mineraloi/university/browser/BaseUITest.java
@@ -2,7 +2,6 @@ package com.lithium.mineraloi.university.browser;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 
-@ExtendWith(BeforeClassExt.class)
-@ExtendWith(AfterClassExt.class)
+@ExtendWith({BeforeClassExt.class, AfterClassExt.class})
 public class BaseUITest {
 }
